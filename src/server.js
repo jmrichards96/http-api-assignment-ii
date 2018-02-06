8244c045910e626bd81d83fbfaef4a0f60dc2629
Josh Richards <jmr8776@rit.edu>
2018-02-05 20:10:36 -0500

    Implemented HTTP API Assignment II.

---
+++ src/server.js
@@ -0,0 +1,97 @@
+const http = require('http'); // pull in http module
+// url module for parsing url string
+const url = require('url');
+// querystring module for parsing querystrings from url
+const query = require('querystring');
+// pull in our custom files
+const responseHandler = require('./responses.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+// handle POST requests
+const handlePost = (request, response, parsedUrl) => {
+  // if post is to /addUser (our only POST url)
+  if (parsedUrl.pathname === '/addUser') {
+    const res = response;
+
+    // uploads come in as a byte stream that we need
+    // to reassemble once it's all arrived
+    const body = [];
+
+    // if the upload stream errors out, just throw a
+    // a bad request and send it back
+    request.on('error', (err) => {
+      console.dir(err);
+      res.statusCode = 400;
+      res.end();
+    });
+
+    // on 'data' is for each byte of data that comes in
+    // from the upload. We will add it to our byte array.
+    request.on('data', (chunk) => {
+      body.push(chunk);
+    });
+
+    // on end of upload stream.
+    request.on('end', () => {
+      // combine our byte array (using Buffer.concat)
+      // and convert it to a string value (in this instance)
+      const bodyString = Buffer.concat(body).toString();
+      // since we are getting x-www-form-urlencoded data
+      // the format will be the same as querystrings
+      // Parse the string into an object by field name
+      const bodyParams = query.parse(bodyString);
+
+      // pass to our addUser function
+      responseHandler.addUser(request, res, bodyParams);
+    });
+  }
+};
+
+// handle GET requests
+const handleGet = (request, response, parsedUrl) => {
+  // route to correct method based on url
+  if (parsedUrl.pathname === '/style.css') {
+    responseHandler.getCSS(request, response);
+  } else if (parsedUrl.pathname === '/getUsers') {
+    responseHandler.getUsers(request, response);
+  } else if (parsedUrl.pathname === '/') {
+    responseHandler.getIndex(request, response);
+  } else {
+    responseHandler.notFound(request, response);
+  }
+};
+
+// handle Head requests
+const handleHead = (request, response, parsedUrl) => {
+  if (parsedUrl.pathname === '/getUsers') {
+    // if get users, send meta data back
+    responseHandler.getUsersMeta(request, response);
+  } else {
+    // if not found send 404 without body
+    responseHandler.notFoundMeta(request, response);
+  }
+};
+
+const onRequest = (request, response) => {
+  // parse url into individual parts
+  // returns an object of url parts by name
+  const parsedUrl = url.parse(request.url);
+
+  switch (request.method) {
+    case 'GET':
+      handleGet(request, response, parsedUrl);
+      break;
+    case 'HEAD':
+      handleHead(request, response, parsedUrl);
+      break;
+    case 'POST':
+      handlePost(request, response, parsedUrl);
+      break;
+    default:
+      // send 404 in any other case
+      responseHandler.notFound(request, response);
+  }
+};
+
+http.createServer(onRequest).listen(port);
